79a83ff5b54f3ceccc3f206ce789ba01458c6a15
Danya <57033366+DanDen04@users.noreply.github.com>
2020-12-29 00:37:34 +0300

    Info-and-Nav
    [ADD}: Info aud 2204, 3205. 3206 and navigation floors

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
-$(function() {
-	$(".selectlink-control").click(function(){
-		var $menu_popup = $(this).next();
-		$menu_popup.slideToggle(200, function(){
-			$('.selectlink ul').not($menu_popup).slideUp(200);
-			if ($menu_popup.is(':hidden')) {
-				$('body').removeClass('body_pointer');
-			} else {
-				$('body').addClass('body_pointer');
-			}					
-		});  
-		return false;
-	});			
+// Изменение цвета path когда мышка над названием магазина.
+$('.info-item').hover(
+	function(){
+		$('.map path[data-id=' + $(this).data('id') + ']').attr('id', 'hover');
+	},
+	function(){
+		$('.map path[data-id=' + $(this).data('id') + ']').attr('id', '');
+	}
+);	
 
-	$(document).on('click', function(e){
-		if (!$(e.target).closest('.selectlink').length){
-			$('body').removeClass('body_pointer');
-			$('.selectlink ul').slideUp(200);
-		}
-	});
+// Клик по названию магазина - открывается подсказка.
+$('.info-item').on('click', function(){
+	$('.info-popup').hide();
+	$('.map path').attr('class', '');
+
+	var popup = $(this).find('.info-popup');
+	$(popup).css('bottom', '-' + ($(popup).outerHeight(true) + 40) + 'px');
+	$(popup).css('left', '-' + (($(popup).outerWidth(true) / 2) - ($(this).outerWidth(true) / 2)) + 'px');
+	$('.info path[data-id=' + $(this).data('id') + ']').attr('class', 'active');
+	$(popup).show();
 });
+
+// Клик по пути магазина - также открывается подсказка.
+$('.map path').click(function(){
+	$('.info-popup').hide();
+	$('.info-item[data-id=' + $(this).data('id') + ']').trigger('click');
+});
+
+// Клик вне магазинов все закрывает.
+$("body").click(function(e) {
+	if ($(e.target).closest(".map path, .info-item").length == 0) {
+		$(".info-popup").hide();
+		$('.map path').attr('class', '');
+	}
+});
+
+// $('body').on('click', function(){
+//     var x=event.pageX;
+//     var y=event.pageY;
+// 	console.log(x,y)
+// })
